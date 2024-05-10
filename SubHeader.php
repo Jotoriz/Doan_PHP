3238876e9dc0f77cac1df738c4ca7cd3359d4064
TrachNguyen <trachnguyen2312@gmail.com>
2024-05-10 17:22:23 +0700

    nguyen_6

--- SubHeader.php
+++ SubHeader.php
@@ -1,38 +1,21 @@
+<?php
+$pdo = new PDO("mysql:host=localhost;dbname=ql_vanphongpham", "root", "");
+$pdo->query("set names utf8");
+// Thực hiện truy vấn để lấy thông tin từ bảng loaisp
+$sql = "SELECT * FROM loaisp";
+$loaiSPs = $pdo->query($sql);
+?>
 <div class="subHeader">
-        <div class="container">
-            <div class="row">
-                <div class="col-2 item">
-                    <img class="hinhsubHeader"
-                        src="https://cdn0.fahasa.com/media/wysiwyg/Thang-05-2024/Icon_VanHoc_120x120.png" />
-                    <p>Chuột</p>
+    <div class="container">
+        <div class="row justify-content-center"> <!-- Đặt lớp justify-content-center để căn giữa các cột -->
+            <?php foreach ($loaiSPs as $loaiSP) : ?>
+                <div class="col-1 item"> <!-- Chia cột thành 8 phần (1/8 của hàng) -->
+                    <img class="hinhsubHeader" src="image/LoaiSP/<?php echo $loaiSP['hinh']; ?>" />
+                    <br> </br>
+                    <p><?php echo $loaiSP['TenLoai']; ?></p>
                 </div>
-                <div class="col-2 item">
-                    <img class="hinhsubHeader"
-                        src="https://cdn0.fahasa.com/media/wysiwyg/Thang-05-2024/Icon_VanHoc_120x120.png" />
-                    <p>Chuột</p>
-                </div>
-                <div class="col-2 item">
-                    <img class="hinhsubHeader"
-                        src="https://cdn0.fahasa.com/media/wysiwyg/Thang-05-2024/Icon_VanHoc_120x120.png" />
-                    <p>Chuột</p>
-                </div>
-                <div class="col-2 item">
-                    <img class="hinhsubHeader"
-                        src="https://cdn0.fahasa.com/media/wysiwyg/Thang-05-2024/Icon_VanHoc_120x120.png" />
-                    <p>Chuột</p>
-                </div>
-                <div class="col-2 item">
-                    <img class="hinhsubHeader"
-                        src="https://cdn0.fahasa.com/media/wysiwyg/Thang-05-2024/Icon_VanHoc_120x120.png" />
-                    <br />
-                    <p>Chuột</p>
-                </div>
-                <div class="col-2 item">
-                    <img class="hinhsubHeader"
-                        src="https://cdn0.fahasa.com/media/wysiwyg/Thang-05-2024/Icon_VanHoc_120x120.png" />
-                    <br />
-                    <p>Chuột</p>
-                </div>
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
+</div>
+
