0050c301bc46aec029ca2a117cc879f40e872d1e
Jotoriz <thientan2408@gmail.com>
2024-05-16 22:41:29 +0700

    Tan_9_Xong_DangNhap_DangKy

--- SubHeader.php
+++ SubHeader.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,17 +13,19 @@
             background-color: #FFB0BD;
             padding: 10px;
         }
+
         .col-1_5 {
             flex: 0 0 10%;
             max-width: 14%;
         }
+
         .item {
             text-align: center;
             margin: 10px;
         }
 
         .hinhsubHeader {
-           
+
             width: 70px;
             height: 70px;
             display: block;
@@ -34,10 +37,11 @@
         }
     </style>
 </head>
+
 <body>
 
     <?php
-    $pdo = new PDO("mysql:host=localhost;port=3307;dbname=ql_vanphongpham", "root", "");
+    $pdo = new PDO("mysql:host=localhost;dbname=ql_vanphongpham", "root", "");
     $pdo->query("set names utf8");
     $sql = "SELECT * FROM loaisp";
     $loaiSPs = $pdo->query($sql);
@@ -45,7 +49,7 @@
     <div class="subHeader">
         <div class="container">
             <div class="row justify-content-center">
-                <?php foreach ($loaiSPs as $loaiSP) : ?>
+                <?php foreach ($loaiSPs as $loaiSP): ?>
                     <div class="col-1_5 item">
                         <img class="hinhsubHeader" src="image/LoaiSP/<?php echo $loaiSP['hinh']; ?>" />
                         <p><?php echo $loaiSP['TenLoai']; ?></p>
@@ -58,6 +62,5 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
+
 </html>
-
-
